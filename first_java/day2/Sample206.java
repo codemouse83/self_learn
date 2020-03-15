9dcd08c970780c87676154dbcd6d20b1ea6477f2
Code Mouse <60502978+codemouse83@users.noreply.github.com>
2020-03-16 07:34:02 +0900

    [add] first_java sample&problem(1,2)

---
+++ first_java/day2/Sample206.java
@@ -0,0 +1,17 @@
+package day2;
+
+public class Sample206 {
+
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+		final int NUMBER = 100;
+		final String STRING = "HOGE";
+		System.out.println(NUMBER);
+		System.out.println(STRING);
+
+		// NUMBER=200; -> error原因 final変数に代入をしようとした。
+
+
+	}
+
+}
---
+++ first_java/problem1/Problem1_1.java
@@ -0,0 +1,11 @@
+package problem1;
+
+public class Problem1_1 {
+
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+		System.out.println("森川　寛也");
+
+	}
+
+}
